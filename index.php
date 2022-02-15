5958eb48be1968d340d0e6ad91a2e79a6d6fb245
Pedro <72272366+P3dro11@users.noreply.github.com>
2022-02-14 20:03:05 -0800

    Update index.php

--- index.php
+++ index.php
@@ -72,7 +72,12 @@
           <p>...</p>
         </div>
       </div>
-     
+     <div id="Interests" class="content-item">
+        <div class="vertical-center">
+          <h2>Interest</h2>
+          <p>...</p>
+        </div>
+      </div>
       
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
