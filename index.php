6caccc631865c50683d254a456bcb4e733c7a06a
Pedro <72272366+P3dro11@users.noreply.github.com>
2022-02-14 19:53:36 -0800

    Update index.php

--- index.php
+++ index.php
@@ -68,7 +68,18 @@
     </div>
      <div id="Skills" class="content-item">
         <div class="vertical-center">
-           
+            <h2>Skills</h2>
+          <?php foreach($json_data['experience'] AS $experience) { ?>
+              <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                  <h3 class="mb-0"><?php echo $experience['title']; ?></h3>
+                  <div class="subheading mb-3"><?php echo $experience['employer']; ?></div>
+                  <p><?php echo $experience['description']; ?></p>
+                </div>
+                <div class="flex-shrink-0">
+                  <span class="text-primary"><?php echo $experience['period']; ?></span>
+                </div>
+              </div>    
      <div id="Interests" class="content-item">
         <div class="vertical-center">
            
