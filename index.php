11b470cd58c02a28e43a71c91ac7447f4c366ed1
Pedro <72272366+P3dro11@users.noreply.github.com>
2022-02-16 12:50:47 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+
+// Read the JSON file
+$json_file = file_get_contents('my_data.json');
+
+// Decode the JSON file
+$json_data = json_decode($json_file,true);
+
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -39,16 +48,6 @@
       </div>
     </div>
     <?php  } ?>
-     
-<?php
-
-// Read the JSON file
-$json_file = file_get_contents('my_data.json');
-
-// Decode the JSON file
-$json_data = json_decode($json_file,true);
-
-?>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
