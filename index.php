8642ec030611ebf22bd4b02b2044f4dfdfc75995
Pedro <72272366+P3dro11@users.noreply.github.com>
2022-02-15 21:11:34 -0800

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,18 @@ $json_data = json_decode($json_file,true);
     <?php echo $json_data['first-name']; ?>
     <span class="text-primary"><?php echo $json_data['last-name']; ?></span>
      </h1>
-    
+    <?php foreach($json_data['experience'] AS $experience) { ?>
+  <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+    <div class="flex-grow-1">
+      <h3 class="mb-0"><?php echo $experience['title']; ?></h3>
+      <div class="subheading mb-3"><?php echo $experience['employer']; ?></div>
+      <p><?php echo $experience['description']; ?></p>
+    </div>
+    <div class="flex-shrink-0">
+      <span class="text-primary"><?php echo $experience['period']; ?></span>
+    </div>
+  </div>
+<?php  } ?>
      
      
 
