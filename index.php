be754e288c81f122f4e15c7e344709eba068d12b
Pedro <72272366+P3dro11@users.noreply.github.com>
2022-02-16 13:41:40 -0800

    Update index.php

--- index.php
+++ index.php
@@ -80,6 +80,13 @@ $json_data = json_decode($json_file,true);
            <p>Programming languages: C++, python, HTML,</p>
         </div>
       </div>
+      <div id="Interests" class="content-item">
+        <div class="vertical-center">
+          <h2>Interests</h2>
+           <p>Things that I find interesting is: soccer, golf, bowling, and biking.</p>
+        </div>
+      </div>
+      
         
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
