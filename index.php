520e3e39684a76f2b9eb56db615f10bd85525385
Pedro <72272366+P3dro11@users.noreply.github.com>
2022-02-16 13:24:47 -0800

    Update index.php

--- index.php
+++ index.php
@@ -50,6 +50,27 @@ $json_data = json_decode($json_file,true);
             <a href="<?php echo $json_data['email']; ?>"><?php echo $json_data['email']; ?></a>
           </div>
           <p><?php echo $json_data['introduction']; ?></p>
+          </div>
+      </div>
+      <hr>
+      <div id="experience" class="content-item">
+        <div class="vertical-center">
+          <h2>Experience</h2>
+          <?php foreach($json_data['experience'] AS $experience) { ?>
+              <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                  <h3 class="mb-0"><?php echo $experience['title']; ?></h3>
+                  <div class="subheading mb-3"><?php echo $experience['employer']; ?></div>
+                  <p><?php echo $experience['description']; ?></p>
+                </div>
+                <div class="flex-shrink-0">
+                  <span class="text-primary"><?php echo $experience['period']; ?></span>
+                </div>
+              </div>
+          <?php  } ?>
+        </div>
+      </div>
+      <hr>
     
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
