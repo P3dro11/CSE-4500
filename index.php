aa152b4ab7f2480446004e05b9c0c7f366ddb9ca
Pedro <72272366+P3dro11@users.noreply.github.com>
2022-02-16 13:11:33 -0800

    Update index.php

--- index.php
+++ index.php
@@ -32,22 +32,22 @@ $json_data = json_decode($json_file,true);
       </nav>
     </div>
     
-  <h1>
-    <?php echo $json_data['first-name']; ?>
-    <span class="text-primary"><?php echo $json_data['last-name']; ?></span>
-</h1>
-     <?php foreach($json_data['experience'] AS $experience) { ?>
-  <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-    <div class="flex-grow-1">
-      <h3 class="mb-0"><?php echo $experience['title']; ?></h3>
-      <div class="subheading mb-3"><?php echo $experience['employer']; ?></div>
-      <p><?php echo $experience['description']; ?></p>
+    <h1>
+      <?php echo $json_data['first-name']; ?>
+      <span class="text-primary"><?php echo $json_data['last-name']; ?></span>
+    </h1>
+      <?php foreach($json_data['experience'] AS $experience) { ?>
+    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+      <div class="flex-grow-1">
+        <h3 class="mb-0"><?php echo $experience['title']; ?></h3>
+        <div class="subheading mb-3"><?php echo $experience['employer']; ?></div>
+        <p><?php echo $experience['description']; ?></p>
+      </div>
+      <div class="flex-shrink-0">
+        <span class="text-primary"><?php echo $experience['period']; ?></span>
+      </div>
     </div>
-    <div class="flex-shrink-0">
-      <span class="text-primary"><?php echo $experience['period']; ?></span>
-    </div>
-  </div>
-<?php  } ?>
+  <?php  } ?>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
